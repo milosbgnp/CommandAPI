7891440b55c5f709b4fbf2dd5040959c905ed33c
Milos <milos.mitrovicbgnp@gmail.com>
2020-08-11 23:53:45 +0200

    Add DbContext and EFC nuget packages

---
+++ Commands/Models/CommandsContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Commands.Models
+{
+    public class CommandsContext : DbContext
+    {
+        public CommandsContext(DbContextOptions<CommandsContext> options) :
+            base(options)
+        {
+
+        }
+        public DbSet<Commands> CommandsItems { get; set; }
+    }
+}
